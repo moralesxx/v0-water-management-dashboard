ca5bc6b3a095b4bbc49f6a4fb2fc5cf1fb9d3485
Fidian Morales <fmoralesp7@miumg.edu.gt>
2026-06-01 21:30:23 -0600

    cambios para el sidebar variable nextjs 16

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -15,7 +15,7 @@ import {
   Zap 
 } from "lucide-react"
 import { cn } from "@/lib/utils"
-import type { ViewType, User } from "@/app/page"
+import type { ViewType } from "@/app/dashboard/page";
 import { Button } from "@/components/ui/button"
 // 🟢 Importamos ambas variantes desde el archivo de logotipos
 import { IconoMenu, IconoMenuAdmin } from '@/components/logos-sistemas';
